26ab09cc757fe897471d5e2d582e2df430db24e2
brad.wu <brad.wu@CN01L0201000296.ebaotech.com>
2016-04-11 12:17:20 +0800

    some changes

--- target/parrot/module/nest-parrot.js
+++ target/parrot/module/nest-parrot.js
@@ -29,7 +29,7 @@
 	};
 
 	// insert all source code here
-	/** nest-parrot.V0.3.0 2016-04-09 */
+	/** nest-parrot.V0.3.0 2016-04-11 */
 (function (window) {
 	var patches = {
 		console: function () {
